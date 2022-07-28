95a5560f5d5aaf52d7aaa4faeace0ef077195740
Kyle Tillson <kyletillson004@gmail.com>
2022-07-28 10:02:39 -0600

    commit tree 096a885e740104a8795ae311e649994e5efe673b

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 function isPalindrome(word) {
+    // iterate from the beginning to the middle
+    for (let i = 0; i < word.length / 2; i++) {
+      // check each letter to the correponding letter from the end
+      const j = word.length - 1 - i;
+      // if any letters don't match, return false
+      if (word[i] !== word[j]) return false;
+    }
+  
+    // return true
+    return true;
   // Write your algorithm here
 }
 
